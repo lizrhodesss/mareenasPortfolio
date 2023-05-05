5aa4ac8689df70f5d888e26fbaeefdbc43214513
liz <rhodes.liz@gmail.com>
2023-05-05 13:47:43 -0400

    hate the way the nav bar looks rn, but got the page scroll to wrk

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const button = document.getElementById('bookButton');
+const navGallery = document.getElementById('navGallery')
+const section = document.getElementById('gallerySection');
+
 
 button.addEventListener('click', () => {
     window.open('https://www.cutlersalon.com/')
 })
+
+navGallery.addEventListener('click', () => {
+    scrollToSection('gallerySection')
+})
+
+const scrollToSection = (id) => {
+    section.scrollIntoView({behavior: 'smooth'})
+}
